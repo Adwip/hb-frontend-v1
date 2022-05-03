64218bdd007dc59d7f4c00f3bc6b490d7ee79342
Agung <prabowoa63@gmail.com>
2022-05-03 22:46:10 +0700

    Constructing base structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM  from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from './pages/Login'
-import Home from './pages/Home'
+// import Login from './pages/Login'
+// import Home from './pages/Home'
 
 ReactDOM.render(<App />, document.getElementById('root'))
